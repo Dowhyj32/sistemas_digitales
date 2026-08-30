dd1cb30f20af3aba5063f16642af3326ad208662
Dowhyj32 <dowhyjlucas@gmail.com>
2026-08-30 16:21:46 +0000

    Taller 2: ej2 & ej3 pasan test

---
+++ taller2/ejercicios/ej3/ff_d.sv
@@ -0,0 +1,19 @@
+module ff_d (
+    input  logic clk,
+    input  logic rst,
+    input  logic d,
+    output logic q
+);
+  
+  always_ff @(posedge clk) begin
+    
+    if (rst) begin
+      q <= 1'b0;
+    
+    end else begin
+      q <= d;
+    end
+
+  end
+  
+endmodule
