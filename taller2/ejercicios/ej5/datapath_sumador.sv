7fa2cd39d7f054ff6453f36ccb82d1d2f73944cc
Dowhyj32 <dowhyjlucas@gmail.com>
2026-09-01 14:26:03 +0000

    Taller2: ej5 pasa test -> TALLER COMPLETO ANDANDO

--- taller2/ejercicios/ej5/datapath_sumador.sv
+++ taller2/ejercicios/ej5/datapath_sumador.sv
@@ -12,4 +12,40 @@ module datapath_sumador (
 );
   // tres registro_4b + sumador_4b (cin=0)
   // R_a y R_b cargan force_in; R_s carga la suma
+
+  logic [3:0] res_sum;
+
+  registro_4b R_a (
+    .clk(clk),
+    .rst(rst),
+    .we (we_a),
+    .din(force_in),
+    .q  (r_a)
+  );
+
+  registro_4b R_b (
+    .clk(clk),
+    .rst(rst),
+    .we (we_b),
+    .din(force_in),
+    .q  (r_b)
+  );
+
+  sumador_4b sumador_4b (
+    .a   (r_a),
+    .b   (r_b),
+    .cin (1'b0),
+    .sum (res_sum),
+    .cout(cout)
+  );
+
+  registro_4b R_s (
+    .clk(clk),
+    .rst(rst),
+    .we (we_s),
+    .din(res_sum),
+    .q  (r_s)
+  );
+
+
 endmodule
